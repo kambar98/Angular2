08849b827001413f41e3e188746a19f7e6987188
kambar98 <kamilbarul123@gmail.com>
2020-08-04 21:00:17 +0200

    components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { SerwerComponent } from './serwer/serwer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SerwerComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/serwer/serwer.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component(
+  {
+    selector: 'app-serwer',
+    templateUrl: './serwer.component.html'
+  })
+export class SerwerComponent {
+
+}
