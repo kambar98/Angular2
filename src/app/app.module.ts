b990473392fc8b4cdff99b42c7d79453ea95fe20
kambar98 <kamilbarul123@gmail.com>
2020-08-11 19:34:04 +0200

    first app configuration

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,23 +3,16 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
-import { SerwerComponent } from './serwer/serwer.component';
-import { SerweryComponent } from './serwery/serwery.component';
-import { WarningComponent } from './warning/warning.component';
-import { SuccessComponent } from './success/success.component';
-import { FormsModule } from '@angular/forms';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    SerwerComponent,
-    SerweryComponent,
-    WarningComponent,
-    SuccessComponent
+
   ],
   imports: [
     BrowserModule,
-    FormsModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
