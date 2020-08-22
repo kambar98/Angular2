c22b926d1550f3d0e57de55023bd7432b80f0825
kambar98 <kamilbarul123@gmail.com>
2020-08-22 13:26:53 +0200

    adding Ingredients to recipe

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,13 +1,20 @@
 import { EventEmitter } from '@angular/core';
  
 import { Recipe } from './recipe.model';
+import { Ingredient } from '../shared/ingredient.model';
 
 
 export class RecipeService {
   recipeSelected = new EventEmitter<Recipe>();
 
   private recipes: Recipe[] = [
-    new Recipe('A test Recipe', 'This is test', 'https://secure.ce-tescoassets.com/assets/PL/108/5900512300108/ShotType1_540x540.jpg')
+    new Recipe('A test Recipe',
+      'This is test',
+      'https://secure.ce-tescoassets.com/assets/PL/108/5900512300108/ShotType1_540x540.jpg'
+      , [
+        new Ingredient('Milk', 1),
+        new Ingredient('fat',20)
+      ])
   ];
 
   getRecipe() {
