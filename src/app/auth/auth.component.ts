bfc4bbe80831d1e77b40d20a283075842d3c8b31
kambar98 <kamilbarul123@gmail.com>
2020-10-01 21:55:37 +0200

    Reflecting the Auth state by login

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -2,6 +2,7 @@ import { Component } from "@angular/core";
 import { NgForm } from "@angular/forms";
 import { AuthService,AuthResponseData } from "./auth.service";
 import { Observable } from "rxjs";
+import { Router } from "@angular/router";
 
 @Component({
   selector: 'app-auth',
@@ -13,7 +14,7 @@ export class AuthComponent {
   isLoading = false;
   error: string = null;
 
-  constructor(private authService: AuthService) { }
+  constructor(private authService: AuthService, private router: Router) { }
 
   onSwitchMode() {
     this.isLoginMode = !this.isLoginMode;
@@ -42,8 +43,8 @@ export class AuthComponent {
     authObs.subscribe(resData => { console.log(resData); this.isLoading = false; }, errorMessage => {
       console.log(errorMessage);
       this.error = errorMessage
-
       this.isLoading = false;
+      this.router.navigate(['/recipes']);
     });
    
     form.reset();
--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,7 +1,8 @@
 import { Injectable } from "@angular/core";
 import { HttpClient, HttpErrorResponse } from "@angular/common/http";
-import { catchError } from "rxjs/operators";
-import { throwError } from "rxjs";
+import { catchError, tap } from "rxjs/operators";
+import { throwError, Subject } from "rxjs";
+import { User } from "./user.model";
  
 
 export interface AuthResponseData {
@@ -16,6 +17,8 @@ export interface AuthResponseData {
 
 @Injectable({ providedIn: 'root' })
 export class AuthService {
+  user = new Subject<User>();
+
   constructor(private http: HttpClient) { }
 
   signup(email: string, password: string) {
@@ -26,8 +29,13 @@ export class AuthService {
       retuSecureToken: true
 
       }
-    ).pipe(catchError(this.handleError)
-      );
+    ).pipe(catchError(this.handleError), tap(resData => {
+      this.handleAuthentication
+        (resData.email,
+          resData.localId,
+          resData.idToken,
+          + resData.expiresIn);
+    }));
   }
 
   login(email: string, password: string) {
@@ -37,7 +45,26 @@ export class AuthService {
         password: password,
         retuSecureToken: true
 
-      }).pipe(catchError(this.handleError));
+      }).pipe(catchError(this.handleError), tap(resData => {
+        this.handleAuthentication
+          (resData.email,
+            resData.localId,
+            resData.idToken,
+            + resData.expiresIn);
+      }));
+  }
+
+  private handleAuthentication(email: string, token: string,userId:string, expiresIn: number) {
+     tap(resData => {
+       const expirationDate = new Date(new Date().getTime() + expiresIn * 1000);
+      const user = new User
+        (email,
+          userId,
+          token,
+          expirationDate);
+      this.user.next(user);
+    }
+    )
   }
 
   private handleError(errorRes: HttpErrorResponse) {
